af42ccae21578d73e26504743ab0af89ab13407f
cobysy <cobysy@users.noreply.github.com>
2018-08-24 12:20:11 +0200

    split into module

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,21 +1,9 @@
 #!/usr/bin/env node
-
-/*
-jconv-ts Copyright (c) 2018 cobysy
-
-Original Python Script Copyright Notice: Copyright (c) 2014,2018 Stuart McGraw
-*/
-
-export type csvtype = 'conj' | 'conjo' | 'conotes' | 'conjo_notes' | 'kwpos';
-export type csvvals = Array<any>;
-export type conjtableitem = Record<string, csvvals>;
-export type conjtables = Record<csvtype, conjtableitem>;
-    
-export class conjugator {
-    public conjugate(kanj: string, kana: string, pos: number, ct: conjtables): Record<string, string> {
-        
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class conjugator {
+    conjugate(kanj, kana, pos, ct) {
         const conjs = this._conjugate(kanj, kana, pos, ct);
-
         // Some conjugations have multiple forms (e.g. ~なくて and ~ないで) that
         // are disinguished by 'onum' in the conjugation key.  The following
         // call will combine these into a single conjugation entry with a text
@@ -23,7 +11,6 @@ export class conjugator {
         this.combine_onums(conjs, ct);
         return conjs;
     }
-
     // Combine multiple conjugation variant "onum" forms of the same
     // conjugation into an a single entry with the onum vaiant texts
     // combined into a single string with "　/　" separating the forms.
@@ -31,21 +18,17 @@ export class conjugator {
     // instead of having keys, (pos,conj,neg,fml,onum) the keys are
     // (pos,conj,neg,fml).
     // We also append any relevant note numbers to the text string here.
-    private combine_onums(conjs: Record<string, string>, ct: conjtables) {
-        const newconjs: Record<string, string> = {};
+    combine_onums(conjs, ct) {
+        const newconjs = {};
         const allnotes = new Set();
-    
         for (const key of Object.keys(conjs).sort()) {
             const [pos, conj, neg, fml, onum] = key.split(','); // js stores key as text
-
             let txt = conjs[key];
             const notes = ct['conjo_notes'][key];
             allnotes.add(notes);
-
             if (notes) {
                 txt += '[' + notes.join(',') + ']';
             }
-
             const newkey = [pos, conj, neg, fml].toString();
             if (!newconjs[newkey]) {
                 newconjs[newkey] = txt;
@@ -56,7 +39,6 @@ export class conjugator {
         }
         return [newconjs, allnotes];
     }
-
     // Generate a dict containing all the conjugated forms of the kanji
     // and/or kana texts 'ktxt' and 'rtxt'.
     // Parameters:
@@ -80,25 +62,24 @@ export class conjugator {
     //   table, see read_csv_files() for more details.
     //   The value of each item is a string with the combined conjugated
     //   form of 'ktxt' and 'rtxt' for that conjugation.
-    private _conjugate(ktxt: string, rtxt: string, pos: number, ct: conjtables): Record<string, string> {
+    _conjugate(ktxt, rtxt, pos, ct) {
         //debug("ktxt: " + ktxt);
         //debug("rtxt: " + rtxt);
-
         // Get pos number from kw
         const sorted = Object.values(ct['conj']).sort((a, b) => {
-            if (a[0] > b[0]) return 1;
-            if (a[0] < b[0]) return -1;
+            if (a[0] > b[0])
+                return 1;
+            if (a[0] < b[0])
+                return -1;
             return 0;
         });
-    
         const negfml = [
             [false, false],
             [false, true],
             [true, false],
-            [true, true]];
-    
-        const conjs: Record<string, string> = {};
-
+            [true, true]
+        ];
+        const conjs = {};
         for (const [conj, conjnm] of sorted) {
             for (const [neg, fml] of negfml) {
                 for (const onum of this.range(1, 10)) {
@@ -108,87 +89,80 @@ export class conjugator {
                     var ctAtIdx;
                     try {
                         ctAtIdx = ct['conjo'][[pos, conj, neg, fml, onum].toString()];
-                    } catch (error) {
+                    }
+                    catch (error) {
                         break;
                     }
                     if (!ctAtIdx) {
                         break;
                     }
-
-                    const stem = ctAtIdx[5] as number;
-                    const okuri = ctAtIdx[6] as string;
-                    const euphr = ctAtIdx[7] as string;
-                    const euphk = ctAtIdx[8] as string;
-                
+                    const stem = ctAtIdx[5];
+                    const okuri = ctAtIdx[6];
+                    const euphr = ctAtIdx[7];
+                    const euphk = ctAtIdx[8];
                     //debug("stem,okuri,euphr,euphk: " + [stem,okuri,euphr,euphk]);
-
                     var kt, rt;
                     if (ktxt) {
                         kt = this.construct(ktxt, stem, okuri, euphr, euphk);
-                    } else {
+                    }
+                    else {
                         kt = '';
                     }
                     if (rtxt) {
                         rt = this.construct(rtxt, stem, okuri, euphr, euphk);
-                    } else {
+                    }
+                    else {
                         rt = '';
                     }
-
                     var txt;
                     if (kt && rt) {
-                        txt = (kt + '【' + rt + '】')
+                        txt = (kt + '【' + rt + '】');
                     }
                     else {
                         txt = kt || rt;
-                    };
-
+                    }
+                    ;
                     conjs[[pos, conj, neg, fml, onum].toString()] = txt;
-
                     //debug("conjugate: " + txt);
                 }
             }
         }
-
         return conjs;
     }
-
     // Given a word (in kanji or kana), generate its conjugated form by
     // by removing removing 'stem' characters from its end (and an additional
     // character if the word is kana and 'euphr' is true or the word is in
     // kanji and 'euphk' are true), then appending either 'euphr' or 'euphk'.
     // We determine if the word is kanji or kana by looking at its next-to-
     // last character.  Finally, 'okuri' is appended.
-    private construct(txt: string, stem: number, okuri: string, euphr: string, euphk: string) {
+    construct(txt, stem, okuri, euphr, euphk) {
         if (txt.length < 2) {
             throw 'ValueError: Conjugatable words must be at least 2 characters long';
         }
-
         //debug("txt: " + txt);
-   
         const checkchar = txt[txt.length - 2][0];
-   
         const iskana = (checkchar >= 'あ'
             && checkchar <= 'ん');
         //debug('iskana: ' + iskana);
- 
         if (iskana && euphr || !iskana && euphk)
             stem += 1;
-    
         //debug('stem:' + stem);
         var ret;
         if (iskana) {
             ret = txt.substring(0, txt.length - stem) + (euphr || '') + (okuri || '');
-        } else {
+        }
+        else {
             ret = txt.substring(0, txt.length - stem) + (euphk || '') + (okuri || '');
         }
         return ret;
     }
-
     // create a generator function returning an
     // iterator to a specified range of numbers
-    private *range(begin: number, end: number, interval: number = 1) {
+    *range(begin, end, interval = 1) {
         for (let i = begin; i < end; i += interval) {
             yield i;
         }
     }
 }
+exports.conjugator = conjugator;
+//# sourceMappingURL=index.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,6 @@
 const gulp = require('gulp');
 const exec = require('child_process').exec;
 
-// folders
-const folder = {
-    src: './',
-    build: './build/'
-};
-
 gulp.task('build-conjtables', function (cb) {
     exec('mkdir -p ./build && cd ./src && node index.js --conjtables > ./../build/conj-tables.json', function (err, stdout, stderr) {
         console.log(stdout);
