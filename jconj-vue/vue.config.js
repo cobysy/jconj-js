b29ddf9e80c6fc52f3ea5cd1c9cc83cd259bb235
cobysy <cobysy@users.noreply.github.com>
2018-08-26 04:18:09 +0200

    deploy

--- jconj-vue/vue.config.js
+++ jconj-vue/vue.config.js
@@ -1,6 +1,10 @@
 // vue.config.js
 module.exports = {
     // options...
+    baseUrl: process.env.NODE_ENV === 'production'
+        ? '/jconj/'
+        : '/',
+    
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
             // https://github.com/mishoo/UglifyJS2/tree/harmony#minify-options
