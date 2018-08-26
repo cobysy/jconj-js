8fb6aa3ef337447291bb1e5af70935660d3fb8c4
cobysy <cobysy@users.noreply.github.com>
2018-08-26 06:10:54 +0200

    update README w/demo site url

--- jconj-vue/vue.config.js
+++ jconj-vue/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // options...
     baseUrl: process.env.NODE_ENV === 'production'
         // ? '/jconj/'
-        ? '/'
+        ? '/jconj/'
         : '/',
     
     configureWebpack: config => {
